f935abea5ab67b9906a2aa22195dd85f45cbaf91
nhatminhp <nmp.tb.212@gmail.com>
2019-11-19 14:44:41 +0700

    add jwt, no token revoked when logout, add env

--- flask-jwt-auth/run.py
+++ flask-jwt-auth/run.py
@@ -1,8 +1,31 @@
 from flask import Flask
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 api = Api(app)
+
+# load dotenv in the base root
+APP_ROOT = os.path.join(os.path.dirname(__file__), '.')   # refers to application_top
+dotenv_path = os.path.join(APP_ROOT, '.env')
+load_dotenv(dotenv_path)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+db = SQLAlchemy(app)
+
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+jwt = JWTManager(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 
 import views, models, resources
 
@@ -13,3 +36,4 @@ api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
 api.add_resource(resources.TokenRefresh, '/token/refresh')
 api.add_resource(resources.AllUsers, '/users')
 api.add_resource(resources.SecretResource, '/secret')
+
